5673e5761685f00176dc88e95544d72c50d9c247
NIYOMUGISHA Obed <obed6@duck.com>
2026-09-02 19:55:07 +0200

    Change page language to "rw"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>IJAMBO</title>
+	<meta name="description" content="Umukino w'amagambo" />
+</svelte:head>
+
+<h1>IJAMBO</h1>
