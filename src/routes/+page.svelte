90d5a05b0fd6cdf454c093fc5ae73ae9885d8632
NIYOMUGISHA Obed <obed6@duck.com>
2026-09-03 06:02:15 +0200

    Add keyboard and sreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,38 @@
 <script lang="ts">
 	import { DESC, TITLE } from '#lib/site.ts';
+	import Keyboard from './Keyboard.svelte';
 </script>
 
 <svelte:head>
 	<title>{TITLE}</title>
 	<meta name="description" content={DESC} />
 </svelte:head>
+
+<div class="wrapper">
+	<section>
+		{#each { length: 30 }}
+			<div></div>
+		{/each}
+	</section>
+	<Keyboard></Keyboard>
+</div>
+
+<style>
+	.wrapper {
+		max-width: 40rem;
+		margin-inline: auto;
+		padding-inline: 1rem;
+	}
+
+	section {
+		display: grid;
+		grid-template-columns: repeat(5, 3.5rem);
+		justify-content: center;
+		gap: 0.25rem;
+
+		div {
+			border: 1.5px solid black;
+			aspect-ratio: 1;
+		}
+	}
+</style>
---
+++ src/routes/Keyboard.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	const rows = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
+</script>
+
+<section>
+	{#each rows as row, index (row)}
+		<div>
+			{const keys = [...row]}
+
+			{#if index == 2}
+				<button>ENTER</button>
+			{/if}
+
+			{#each keys as key (key)}
+				<button>{key}</button>
+			{/each}
+
+			{#if index == 2}
+				<button>⌫</button>
+			{/if}
+		</div>
+	{/each}
+</section>
+
+<style>
+	section {
+		margin-block-start: 1.5rem;
+	}
+
+	div {
+		display: flex;
+		gap: 0.25rem;
+
+		&:not(:first-child) {
+			margin-block-start: 0.5rem;
+		}
+	}
+
+	button {
+		flex-grow: 1;
+		height: 2rem;
+		color: white;
+		background-color: #515151;
+		border-radius: 0.4rem;
+		border: none;
+		cursor: pointer;
+		font-weight: 700;
+	}
+</style>
